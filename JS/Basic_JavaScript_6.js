5ae8a65f10743bbe6e07b7b143e5e770f399867f
calebblack96 <57781987+calebblack96@users.noreply.github.com>
2019-12-22 11:29:08 -0800

    Fix Syntax

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -11,9 +11,9 @@ function utilize(elem){
 
 function count_Function() {
 	document.getElementById("Counting").innerHTML = Count();
-	function.Count() {
+	function Count() {
 		var Starting_point = 9;
-		function Plus_one() (Starting_point += 1;)
+	function Plus_one() {Starting_point += 1;}
 		Plus_one();
 		return Starting_point;	
 	}
