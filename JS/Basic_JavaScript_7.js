4980d2398450519a40491d2c21aea7b0a038a0af
calebblack96 <57781987+calebblack96@users.noreply.github.com>
2019-12-31 10:32:39 -0800

    Corrected

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -13,8 +13,8 @@ document.getElementById("Words").innerHTML = Ride;
 }
 
 
-function get_Date() {
-	if (new Date()getHours() < 18) {
+function get_Date(){
+	if (new Date().getHours() < 18) {
 		document.getElementById("Greeting").innerHTML = "How are you today?";
 	}
 }
@@ -25,7 +25,7 @@ function Time_function(){
 	if (Time < 12 == Time > 0) {
 		Reply = "It is the morning.";
 	}
-	}
+	
 	else if (Time > 12 == Time < 10) {
 		Reply= "It is te afternoon.";
 	}
@@ -36,18 +36,3 @@ function Time_function(){
 
 }
 
-function Time_function(){
-	var Time = new Date().getHours();
-	var Reply;
-	if (Time < 12 == Time > 0) {
-		Reply = "It is the morning.";
-	}
-	else if (Time > 12 == Time < 10) {
-		Reply= "It is te afternoon.";
-	}
-	else {
-		Reply = "It is the evening.";
-	}
-	document.getElementById("Time_of_day").innerHTML = Reply;
-
-}
